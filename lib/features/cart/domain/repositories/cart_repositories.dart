ab04586000dceb6c8e4277ba1178070fae56fddc
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-02-27 20:01:40 +0700

    membuat cart_repositories

---
+++ lib/features/cart/domain/repositories/cart_repositories.dart
@@ -0,0 +1,8 @@
+import 'package:stateman_with_provider/features/cart/domain/entities/product.dart';
+
+abstract class CartRepositories {
+  List<Product> getCartItems();
+  void addItem(Product product);
+  void removeAllItems();
+  bool isInCart(String productid);
+}
