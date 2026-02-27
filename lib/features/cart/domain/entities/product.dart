a32426003db9e5b2f02574baf4ba5c1eb6ca48b1
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-02-27 19:58:42 +0700

    membuat entitas untuk product

---
+++ lib/features/cart/domain/entities/product.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String id;
+  final String name;
+  final double price;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+}
